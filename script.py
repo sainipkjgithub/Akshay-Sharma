94ffb4ccfaffbcb8dad1326aa763fa52605fba3f
sainipkjgithub <160867713+sainipkjgithub@users.noreply.github.com>
2025-03-26 18:07:09 +0530

    Add files via upload

---
+++ script.py
@@ -0,0 +1,8 @@
+from dotenv import load_dotenv
+import os
+load_dotenv()
+FILE_CHANNEL_ID = int(os.getenv("FILE_CHANNEL_ID"))
+API_ID = int(os.getenv("API_ID"))
+API_HASH = os.getenv("API_HASH")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+SEARCH_URL = "https://sainipankaj12.serv00.net/App/index.php?query="
