9fa7673823f93a997aa146745383b39095b8f0f1
sainipkjgithub <160867713+sainipkjgithub@users.noreply.github.com>
2025-03-27 12:21:48 +0530

    Update script.py

--- script.py
+++ script.py
@@ -6,3 +6,5 @@ API_ID = int(os.getenv("API_ID"))
 API_HASH = os.getenv("API_HASH")
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 SEARCH_URL = "https://sainipankaj12.serv00.net/App/index.php?query="
+user_status = {}
+admin_app_details = {}
