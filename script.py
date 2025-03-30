5c5d4b58f82f84a0225f5f57bba8af183d98a8df
sainipkjgithub <160867713+sainipkjgithub@users.noreply.github.com>
2025-03-30 12:17:34 +0530

    Update script.py

--- script.py
+++ script.py
@@ -8,3 +8,7 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 SEARCH_URL = "https://sainipankaj12.serv00.net/App/index.php?query="
 user_status = {}
 admin_app_details = {}
+user_histories = {}
+temp_data = {}
+SEARCH_URL = "https://sainipankaj12.serv00.net/App/Pre/get.php?query="
+admins = {6150091802: "Owner", 5943119285: "Admin"}  # Example Admin Dictionary
